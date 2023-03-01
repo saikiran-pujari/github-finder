62bc0923d01ff1997f3b35c9f5c2393c28f80e95
Saikiran Pujari <apple@appurunoMacBook-Air.local>
2023-03-01 10:17:15 +0530

    edited user

--- src/components/layout/users/User.js
+++ src/components/layout/users/User.js
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
+import Spinner from "../Spinner";
+import PropTypes from "prop-types";
 
 class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
   }
+
+  static propTypes = {
+    loading: PropTypes.bool,
+    user: PropTypes.object.isRequired,
+    getUser: PropTypes.func.isRequired,
+  };
+
   render() {
     const {
       name,
