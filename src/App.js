b54c5976efc6921a961a52475ac24c9a520c68b0
Saikiran Pujari <apple@appurunoAir.Dlink>
2023-03-02 16:31:34 +0530

    repos and repositems component

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
   state = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
     alert: null,
   };
@@ -33,6 +34,15 @@ class App extends Component {
     this.setState({ user: res.data, loading: false });
   };
 
+  // Get users repos
+  getUserRepos = async (username) => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ repos: res.data, loading: false });
+  };
+
   // Clear users
   clearUsers() {
     this.setState({ users: [], loading: false });
@@ -44,7 +54,7 @@ class App extends Component {
     setTimeout(() => this.setState({ alert: null }), 5000);
   }
   render() {
-    const { users, user, loading } = this.state;
+    const { users, user, loading, repos } = this.state;
     return (
       <Router>
         <div className="App">
@@ -75,7 +85,9 @@ class App extends Component {
                   <User
                     {...props}
                     getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
                     user={user}
+                    repos={repos}
                     loading={loading}
                   />
                 )}
--- src/components/layout/users/User.js
+++ src/components/layout/users/User.js
@@ -2,16 +2,20 @@ import React, { Component, Fragment } from "react";
 import Spinner from "../Spinner";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import Repos from "../../repos/Repos";
 
 class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
+    this.props.getUserRepos(this.props.match.params.login);
   }
 
   static propTypes = {
     loading: PropTypes.bool,
     user: PropTypes.object.isRequired,
+    repos: PropTypes.array.isRequired,
     getUser: PropTypes.func.isRequired,
+    getUserRepos: PropTypes.func.isRequired,
   };
 
   render() {
@@ -96,21 +100,15 @@ class User extends Component {
               </ul>
             </div>
           </div>
-
           <div className="card text-center">
-            <div className="badge badge-primary">
-              Followers: {followers}
-            </div>
-            <div className="badge badge-success">
-              Following: {following}
-            </div>
+            <div className="badge badge-primary">Followers: {followers}</div>
+            <div className="badge badge-success">Following: {following}</div>
             <div className="badge badge-light">
               Public Repos: {public_repos}
             </div>
-            <div className="badge badge-dark">
-              Public Gists: {public_gists}
-            </div>
+            <div className="badge badge-dark">Public Gists: {public_gists}</div>
           </div>
+          <Repos repos={this.props.repos} />
         </Fragment>
       </div>
     );
