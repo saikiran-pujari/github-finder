fa3a3e655d2125baddd09ad426962985fcb47c0a
Saikiran Pujari <apple@appurunoAir.Dlink>
2023-03-07 10:37:58 +0530

    GitStatus

--- src/context/GithubState.js
+++ src/context/GithubState.js
@@ -22,6 +22,16 @@ const GithubState = (props) => {
   const [state, dispatch] = useReducer(GithubReducer, initialState);
 
   //   Search User
+  const searchUsers = async (text) => {
+    setLoading();
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    dispatch({
+      type: SEARCH_USERS,
+      payload: res.data,
+    });
+  };
 
   // Get user
 
@@ -30,6 +40,7 @@ const GithubState = (props) => {
   // Clear Users
 
   // set Loading
+  const setLoading = () => dispatch({ type: SET_LOADING });
 
   return (
     <GithubContext.Provider
